622024f5a23588fed418981a0ccf38152dcb1cc2
Md. Touhid Meher <mdtouhidmeher@gmail.com>
2026-01-27 21:38:10 +0600

    27-1

---
+++ a.py
@@ -0,0 +1,10 @@
+n, d = map(int, input().split())
+a = list(map(int, input().split()))
+moves = 0
+for i in range(1, n):
+    if a[i] <= a[i-1]:
+        diff = a[i-1] - a[i] + 1
+        k = (diff + d - 1) // d
+        a[i] += k * d
+        moves += k
+print(moves)
